0a99b0a72aaabe5f644565172eed941c01d80c53
Denis Kraev <deniskraevcc@gmail.com>
2023-04-27 18:32:20 +0300

    user controller added

---
+++ src/controllers/users.ts
@@ -0,0 +1,106 @@
+import { Request, Response, NextFunction } from 'express';
+import { default as ErrorHandler } from '../errors/errors';
+import User from '../models/user';
+
+
+class UserController {
+  async createUser(req: Request, res: Response, next: NextFunction) {
+    const { name, about, avatar } = req.body;
+    if (!name || !about || !avatar) {
+      return next(ErrorHandler.badRequest('incorrect user data'));
+    }
+    try {
+      const user = await User.create({ name, about, avatar });
+      return res.json({ data: user });
+    } catch (error) {
+      console.error(error);
+      next(ErrorHandler.internal('server error'));
+    }
+  }
+
+  async getUserById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        throw ErrorHandler.authorization('user not found');
+      }
+      res.json({ data: user });
+    } catch (error: unknown) {
+      // @ts-ignore
+      if (error instanceof ErrorHandler) {
+        return next(error);
+      }
+      console.error(error);
+      next(ErrorHandler.internal('server error'));
+    }
+  }
+
+  async getAllUsers(req: Request, res: Response, next: NextFunction) {
+    try {
+      const users = await User.find({});
+      return res.json({ data: users });
+    } catch (error) {
+      console.error(error);
+      next(ErrorHandler.internal('server error'));
+    }
+  }
+
+  async updateUserInfo(req: any, res: Response, next: NextFunction) {
+    const { name, about } = req.body;
+    const id = req.user?._id;
+
+    try {
+      if (!name || !about) {
+        return next(ErrorHandler.badRequest('incorrect data'));
+      }
+      const user = await User.findByIdAndUpdate(
+        id,
+        {
+          name,
+          about,
+        },
+        {
+          new: true,
+          runValidators: true,
+        },
+      );
+      if (!user) {
+        return next(ErrorHandler.authorization('user not found'));
+      }
+      return res.json({ data: user });
+    } catch (error) {
+      console.log(error);
+      next(ErrorHandler.internal('server error'));
+    }
+  }
+
+  async updateUserAvatar(req: any, res: Response, next: NextFunction) {
+    const avatar = req.body;
+    const id = req.user?._id;
+
+    try {
+      if (!avatar) {
+        return next(ErrorHandler.badRequest('incorrect data'));
+      }
+      const user = await User.findByIdAndUpdate(
+        id,
+        {
+          avatar,
+        },
+        {
+          new: true,
+          runValidators: true,
+        },
+      );
+      if (!user) {
+        return next(ErrorHandler.authorization('user not found'));
+      }
+      return res.json({ data: user });
+    } catch (error) {
+      console.error(error);
+      next(ErrorHandler.internal('server error'));
+    }
+  }
+}
+
+export default new UserController();
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,14 +1,20 @@
-export const nameValidationOptions = {
+interface ValidationOptions {
+  // eslint-disable-next-line no-unused-vars
+  validator: (arg: string) => boolean;
+  message: string;
+}
+
+export const nameValidationOptions: ValidationOptions = {
   validator: (str: string) => str.length > 2 && str.length <= 30,
   message: 'username must be between 2 and 30 characters',
 };
 
-export const aboutValidationOptions = {
+export const aboutValidationOptions: ValidationOptions = {
   validator: (str: string) => str.length > 2 && str.length <= 200,
   message: 'about info must be between 2 and 200 characters',
 };
 
-export const linkValidationOptions = {
+export const linkValidationOptions: ValidationOptions = {
   validator: (str: string) => /^http(s)?:\/\/\S+(\.jpg|\.jpeg|\.png)$/.test(str),
-  message: 'avatar must be a valid URL to a JPG or PNG image',
+  message: 'avatar must be a valid URL',
 };
