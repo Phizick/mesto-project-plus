64f99d4b269f4e9fa13689240cbc2ffeeeccc428
Denis Kraev <deniskraevcc@gmail.com>
2023-04-27 19:52:26 +0300

    test user added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,31 @@
 import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import router from './routes/index';
 
+const PORT = 3503;
 const app: Application = express();
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.use(express.json());
+app.use('/', router);
+
+app.use((req: any, res, next) => {
+  req.user = {
+    _id: '644aa6b337ae86e426ed79a2',
+  };
+
+  next();
 });
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    app.listen(
+      PORT,
+      () => console.log(`Server started on port ${PORT}`),
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
