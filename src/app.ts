ff5ace730d74d736f93211d5b0fea8138ba6a54a
Denis Kraev <deniskraevcc@gmail.com>
2023-05-02 15:42:46 +0300

    final

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
 import mongoose from 'mongoose';
+import { errors } from 'celebrate';
 import router from './routes/index';
 import errorMiddleware from './middlewares/errorMiddleware';
 import UserController from './controllers/users';
 import authMiddleware from './middlewares/auth';
 import { requestLogger } from './middlewares/requestLogger';
 import { errorLogger } from './middlewares/errorLogger';
+import { createUserValidation, loginValidation } from './validation/userValidation';
 
 require('dotenv').config();
 
@@ -16,11 +18,12 @@ app.use(express.json());
 
 app.use(requestLogger);
 
-app.post('/signin', UserController.login);
-app.post('/signup', UserController.createUser);
+app.post('/signin', loginValidation, UserController.login);
+app.post('/signup', createUserValidation, UserController.createUser);
 app.use(authMiddleware);
 app.use('/', router);
 app.use(errorLogger);
+app.use(errors());
 app.use(errorMiddleware);
 
 const start = async () => {
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import UserController from '../controllers/users';
+import {
+  getUserByIdValidation,
+  updateUserAvatarValidation,
+  updateUserInfoValidation,
+} from '../validation/userValidation';
 
 const router = Router();
 
 router.get('/', UserController.getAllUsers);
-router.get('/:id', UserController.getUserById);
+router.get('/:id', getUserByIdValidation, UserController.getUserById);
 router.get('/me', UserController.getUserInfo);
-router.patch('/me', UserController.updateUserInfo);
-router.patch('/me/avatar', UserController.updateUserAvatar);
+router.patch('/me', updateUserInfoValidation, UserController.updateUserInfo);
+router.patch('/me/avatar', updateUserAvatarValidation, UserController.updateUserAvatar);
 export default router;
