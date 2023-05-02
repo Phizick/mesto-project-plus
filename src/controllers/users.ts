3b5e40c84d4ef358f73fae4c861774898de9e860
Denis Kraev <deniskraevcc@gmail.com>
2023-05-02 10:25:24 +0300

    userController update

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,16 +1,37 @@
 import { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
 import ErrorHandler from '../errors/errors';
 import User from '../models/user';
 
 class UserController {
   async createUser(req: Request, res: Response, next: NextFunction) {
-    const { name, about, avatar } = req.body;
-    if (!name || !about || !avatar) {
+    const {
+      name = 'Жак-Ив-Кусто',
+      about = 'Исследователь',
+      avatar = 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+      email,
+      password,
+    } = req.body;
+    if (!email || !password) {
       return next(ErrorHandler.badRequest('incorrect user data'));
     }
     try {
-      const user = await User.create({ name, about, avatar });
-      return res.json({ data: user });
+      const uniqueUser = await User.findOne({ email });
+      if (uniqueUser) {
+        return next(ErrorHandler.badRequest('incorrect user data'));
+      }
+      const hashPassword = await bcrypt.hash(password, 12);
+      const user = await User.create({
+        name, about, avatar, email, password: hashPassword,
+      });
+      return res.json({
+        data: {
+          name: user.name,
+          about: user.about,
+          avatar: user.avatar,
+          email: user.email,
+        },
+      });
     } catch (error) {
       console.error(error);
       next(ErrorHandler.internal('На сервере произошла ошибка'));
