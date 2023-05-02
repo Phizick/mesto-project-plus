ff238b1f97e01fb1e4aab0ad8f66f81c960c65da
Denis Kraev <deniskraevcc@gmail.com>
2023-05-02 09:41:16 +0300

    userSchema required data fixes

--- src/models/user.ts
+++ src/models/user.ts
@@ -19,19 +19,16 @@ const UserSchema = new Schema<IUser>({
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: [true, 'name is required'],
     validate: nameValidationOptions,
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 200,
-    required: [true, 'about info is required'],
     validate: aboutValidationOptions,
   },
   avatar: {
     type: String,
-    required: [true, 'avatar is required'],
     validate: linkValidationOptions,
   },
   email: {
